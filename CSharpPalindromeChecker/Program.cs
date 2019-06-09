19b0da0e221a606c31a246b3ddce5b9707a1b6b5
Christian Higgins <christian.higgins@gmail.com>
2019-06-08 20:57:00 -0400

    Madam, I'm Adam!

--- CSharpPalindromeChecker/Program.cs
+++ CSharpPalindromeChecker/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace CSharpPalindromeChecker
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        Start();
+    }
+
+    static void Start()
+    {
+        Console.WriteLine("Please enter a string and I will check to see if it is a palindrome.");
+        string userInput = Console.ReadLine();
+        if (PalindromeChecker(userInput))
         {
+            Console.WriteLine("Yep, that's a palindrome!");
         }
+        else
+        {
+            Console.WriteLine("No, not a palindrome!");
+        }
+
+
+        Console.ReadLine();
+    }
+
+    static string ReverseString(string str)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            sb.Append(str[i]);
+        }
+
+        return sb.ToString();
+    }
+
+    static string RemoveSpecialCharacters(string str)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.Length; i++)
+        {
+            if ((str[i] >= 'A' && str[i] <= 'z'))
+            {
+                sb.Append(str[i]);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    static bool PalindromeChecker(string str)
+    {
+        return (RemoveSpecialCharacters(str).ToLower() == ReverseString(RemoveSpecialCharacters(str).ToLower()));
     }
 }
